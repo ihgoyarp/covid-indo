7f33825161ecd6438d46732e1c1923f22eff38ad
ihgoyarp <prayoghi.indo@gmail.com>
2022-04-30 03:14:01 +0800

    masih susah

---
+++ covid.py
@@ -0,0 +1,13 @@
+import requests
+import json
+response_API = requests.get(
+    'https://data.covid19.go.id/public/api/update.json')
+#print(response_API.status_code)
+data = response_API.text
+parse_json = json.loads(data)
+
+active_case = parse_json['update']['harian']
+print(json.dumps(parse_json, indent=4))
+
+
+
